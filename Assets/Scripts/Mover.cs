9b4f218c9b97cbec5d3620730d3f52d64eba071a
alierenkarateke <alierenkarateke.offical@gmail.com>
2025-11-17 20:21:06 +0300

    v0.1 Update

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical")   * moveSpeed * Time.deltaTime;
+    
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
